b7b9eba871cf91e122ad0230398bf2113eff034d
Henry Ng <heng@tesla.com>
2017-07-17 21:43:10 -0700

    clean up

--- app/forms/form-list.jsx
+++ app/forms/form-list.jsx
@@ -37,7 +37,6 @@ export class FormList extends Component {
   }
 
   submitHandler(formState) {
-    console.log(formState)
     fetch('/createOrUpdateTask', this.createPostObject(formState))
       .then(this.getAllTasks.bind(this))
       .catch(function(err) {
